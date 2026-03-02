660a3185ad3d74476ff1d972f6a5f6374c5fe549
salsnablh <salsnabllh@gmail.com>
2026-03-02 14:38:03 +0700

    Menambah logo

--- application/views/admin/header.php
+++ application/views/admin/header.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>SB Admin 2 - Dashboard</title>
+    <title>Santika - Aplikasi Absensi</title>
 
     <!-- Custom fonts for this template-->
     <link href="<?= base_url('assets/vendor/fontawesome-free/css/all.min.css') ?>" rel="stylesheet">
@@ -19,6 +19,7 @@
 
     <!-- Custom styles for this template-->
     <link href="<?= base_url('assets/css/sb-admin-2.min.css') ?>" rel="stylesheet">
+    <link rel="icon" type="image/x-icon" href="<?= base_url('favicon.ico') ?>">
 
 </head>
 
